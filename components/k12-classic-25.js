c6e7e14ff9305977828f81eaa79cce5b2cf72c53
Kylian1jona <jojostvvidz2020@gmail.com>
2026-07-26 22:01:42 -0500

    Replace filler distractors with four sensible choices

--- components/k12-classic-25.js
+++ components/k12-classic-25.js
@@ -3,7 +3,7 @@
 (function(){
   const loadedGrades=new Set();
   const pendingGrades=new Map();
-  const release="classic25-20260726.10";
+  const release="classic25-20260726.11";
 
   function cloneQuestion(question){
     if(typeof structuredClone==="function") return structuredClone(question);
